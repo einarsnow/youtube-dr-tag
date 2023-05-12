dfd090e44aa5a1374c5c8ee07ec49b37dfefc671
einarsnow <einarsnow@gmail.com>
2023-05-13 03:36:53 +0700

    Change GM_addStyle to GM.addStyle

--- youtube-dr-tag.user.js
+++ youtube-dr-tag.user.js
@@ -1,25 +1,16 @@
 // ==UserScript==
 // @name         YouTube Dr. Tag
 // @namespace    einarsnow
-// @version      1.1
+// @version      1.2
 // @author       einarsnow
 // @description  Tag user in YouTube live chat by click
 // @updateURL    https://github.com/einarsnow/youtube-dr-tag/raw/main/youtube-dr-tag.user.js
 // @downloadURL  https://github.com/einarsnow/youtube-dr-tag/raw/main/youtube-dr-tag.user.js
 // @supportURL   https://github.com/einarsnow/youtube-dr-tag
 // @match        https://www.youtube.com/live_chat*
-// @grant        GM_addStyle
 // @grant        GM.addStyle
 // ==/UserScript==
 
-GM_addStyle(`
-    #author-name {
-        cursor: pointer;
-    }
-    #author-name:hover {
-        text-decoration: underline;
-    }
-`)
 GM.addStyle(`
     #author-name {
         cursor: pointer;
@@ -34,7 +25,12 @@ chat.addEventListener("click", function(e) {
     if (e.target.id != "author-name") return
 
     let input = document.querySelector("#input[contenteditable]")
-    input.innerText = `@${e.target.innerText}\xa0${input.innerText}`
+    if (input.innerText.length == 0) {
+      input.innerText = `@${e.target.innerText}\xa0`
+    } else {
+      input.innerText = `${input.innerText}\xa0@${e.target.innerText}\xa0`
+      input.innerText = input.innerText.replaceAll("\xa0\xa0@", "\xa0@")
+    }
     input.dispatchEvent(new Event("input"))
 
     const range = document.createRange()
