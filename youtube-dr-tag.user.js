e5908b84095debbb601166ded1afbb688fdebb58
einarsnow <einarsnow@gmail.com>
2023-05-13 02:28:31 +0700

    Replace tag to end

--- youtube-dr-tag.user.js
+++ youtube-dr-tag.user.js
@@ -25,7 +25,7 @@ chat.addEventListener("click", function(e) {
     if (e.target.id != "author-name") return
 
     let input = document.querySelector("#input[contenteditable]")
-    input.innerText = `@${e.target.innerText}\xa0${input.innerText}`
+    input.innerText = `${input.innerText}\xa0@${e.target.innerText}`
     input.dispatchEvent(new Event("input"))
 
     const range = document.createRange()
